8d1561560631f5592b31333df4fce5d878458faf
munyaradzi-ops <mangondomunyaradzij@gmail.com>
2026-08-26 00:04:54 +0200

    Configure WhiteNoise to explicitly serve media files directory

--- config/wsgi.py
+++ config/wsgi.py
@@ -1,16 +1,12 @@
-"""
-WSGI config for config project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/5.2/howto/deployment/wsgi/
-"""
-
 import os
-
 from django.core.wsgi import get_wsgi_application
+from whitenoise import WhiteNoise
+from django.conf import settings
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
 
+# 1. Initialize the standard Django application layout
 application = get_wsgi_application()
+
+# 2. Wrap your application with WhiteNoise and explicitly point it to your media folder
+application = WhiteNoise(application, root=settings.MEDIA_ROOT, prefix=settings.MEDIA_URL)
