b23d6089d2b13b830e6bc3d1bff6d0b5d19f3eec
munyaradzi-ops <mangondomunyaradzij@gmail.com>
2026-08-26 10:24:47 +0200

    chore: resolve merge conflict in settings.py

--- config/settings.py
+++ config/settings.py
@@ -157,3 +157,25 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+LOGGING = {
+    "version": 1,
+    "disable_existing_loggers": False,
+    "handlers": {
+        "console": {
+            "class": "logging.StreamHandler",
+        },
+    },
+    "root": {
+        "handlers": ["console"],
+        "level": "WARNING",
+    },
+    "loggers": {
+        "django": {
+            "handlers": ["console"],
+            "level": "ERROR",
+            "propagate": False,
+        },
+    },
+}
+
